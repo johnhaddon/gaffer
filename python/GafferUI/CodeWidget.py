94df3e4a6e1a3f2d6649381cfbf2d0dbbd266971
John Haddon <thehaddonyoof@gmail.com>
2020-04-25 13:14:05 +0100

    highlighting wip

--- python/GafferUI/CodeWidget.py
+++ python/GafferUI/CodeWidget.py
@@ -37,11 +37,19 @@
 import os
 import re
 import functools
+import token
+import keyword
+import tokenize
+import io
+
+import imath
 
 import IECore
 
 import Gaffer
 import GafferUI
+
+from Qt import QtGui
 
 class CodeWidget( GafferUI.MultiLineTextWidget ) :
 
@@ -61,47 +69,7 @@ class CodeWidget( GafferUI.MultiLineTextWidget ) :
 
 		return self.__completer
 
-	def __keyPress( self, widget, event ) :
-
-		if self.__completer is None or event.key != "Tab" :
-			return False
-
-		text = self.getText()
-		cursor = self.getCursorPosition()
-		lineStart = max( text.rfind( "\n", 0, cursor ), 0 )
-		line = text[lineStart:cursor]
-
-		if not line.strip() :
-			return False
-
-		completions = self.__completer( line )
-		if not completions :
-			return False
-
-		commonPrefix = os.path.commonprefix( completions )
-		if len( commonPrefix ) > len( line ) :
-			self.insertText( commonPrefix[len(line):] )
-		else :
-			menuDefinition = IECore.MenuDefinition()
-			for i, c in enumerate( completions ) :
-				menuDefinition.append(
-					"/{}".format( i ),
-					{
-						"label" : c,
-						"command" : functools.partial(
-							Gaffer.WeakMethod( self.insertText ), c[len(line):]
-						)
-					}
-				)
-			self.__completionMenu = GafferUI.Menu( menuDefinition )
-			self.__completionMenu.popup(
-				parent = self.ancestor( GafferUI.Window ),
-				position = self.cursorBound().max(),
-				grabFocus = False
-			)
-
-		return True
-
+	# MAKE BASE CLASS
 	class PythonCompleter( object ) :
 
 		__searchPrefix = r"(?:^|(?<=[\s,(]))"
@@ -188,4 +156,124 @@ class CodeWidget( GafferUI.MultiLineTextWidget ) :
 					result.append( prefix + namePrefix + name + nameSuffix )
 
 			return sorted( result )
+
+	# HIDE IMPLEMENTATION
+	class PythonHighlighter( QtGui.QSyntaxHighlighter ) :
+
+		#@staticmethod
+		def __format( color ) :
+
+			f = QtGui.QTextCharFormat()
+			f.setForeground( GafferUI.Widget._qtColor( color ) )
+			return f
+
+		__keywordFormat = __format( imath.Color3f( 64 / 255., 156/255., 219/255. ) )
+		__stringFormat = __format( imath.Color3f( 216 / 255., 146/255., 115/255. ) )
+		__commentFormat = __format( imath.Color3f( 90 / 255., 156/255., 76/255. ) )
+		__controlFlowFormat = __format( imath.Color3f( 207 / 255., 128 / 255., 195 / 255. ) )
+		__numberFormat = __format( imath.Color3f( 174 / 255., 208 / 255., 164 / 255. ) )
+		__callFormat = __format( imath.Color3f( 219 / 255., 221 / 255., 164 / 255. ) )
+		__operatorFormat = __format( imath.Color3f( 201 / 255., 0, 28/255. ) )
+
+		__controlFlowKeywords = {
+			"if", "elif", "else",
+			"try", "except", "finally",
+			"for", "while",
+			"from", "import",
+			"return",
+		}
+
+		__parenthesis = {
+			"(", ")", "[", "]", "{", "}",
+		}
+
+		__operators = {
+			"=", "==", "!=",
+			"+", "+=", "-", "-=", "*", "*=", "/", "/=", "//", "//=", "%", "%=",
+			"|", "|=", "&", "&=", "^", "^=", "~",
+			">", ">=", "<", "<=", "**", "<<", ">>",
+		}
+
+		def highlightBlock( self, text ) :
+
+			f = QtGui.QTextCharFormat()
+			f.setForeground( QtGui.QColor( 255, 0, 0 ) )
+
+			g = QtGui.QTextCharFormat()
+			g.setForeground( QtGui.QColor( 0, 255, 0 ) )
+
+			print text
+			pendingName = None
+			with IECore.IgnoredExceptions( tokenize.TokenError ) :
+				for t in tokenize.generate_tokens( io.StringIO( text ).readline ) :
+					f = None
+					if t[0] == token.NAME :
+						if t[1] in self.__controlFlowKeywords :
+							f = self.__controlFlowFormat
+						elif keyword.iskeyword( t[1] ) or t[1] == "self" :
+							f = self.__keywordFormat
+						else :
+							pendingName = t
+							continue
+					elif t[0] == token.STRING :
+						f = self.__stringFormat
+					elif t[0] == tokenize.COMMENT :
+						f = self.__commentFormat
+					elif t[0] == token.NUMBER :
+						f = self.__numberFormat
+					elif t[0] == token.OP :
+						if t[1] in self.__parenthesis :
+							f = self.__callFormat
+							if t[1] == "(" :
+								if pendingName is not None :
+									self.setFormat( pendingName[2][1], pendingName[3][1] - pendingName[2][1], self.__callFormat )
+						elif t[1] in self.__operators :
+							f = self.__operatorFormat
+
+					if f is not None  :
+						self.setFormat( t[2][1], t[3][1] - t[2][1], f )
+
+					pendingName = None
+
+	def __keyPress( self, widget, event ) :
+
+		if self.__completer is None or event.key != "Tab" :
+			return False
+
+		text = self.getText()
+		cursor = self.getCursorPosition()
+		lineStart = max( text.rfind( "\n", 0, cursor ), 0 )
+		line = text[lineStart:cursor]
+
+		if not line.strip() :
+			return False
+
+		completions = self.__completer( line )
+		if not completions :
+			return False
+
+		commonPrefix = os.path.commonprefix( completions )
+		if len( commonPrefix ) > len( line ) :
+			self.insertText( commonPrefix[len(line):] )
+		else :
+			menuDefinition = IECore.MenuDefinition()
+			for i, c in enumerate( completions ) :
+				menuDefinition.append(
+					"/{}".format( i ),
+					{
+						"label" : c,
+						"command" : functools.partial(
+							Gaffer.WeakMethod( self.insertText ), c[len(line):]
+						)
+					}
+				)
+			self.__completionMenu = GafferUI.Menu( menuDefinition )
+			self.__completionMenu.popup(
+				parent = self.ancestor( GafferUI.Window ),
+				position = self.cursorBound().max(),
+				grabFocus = False
+			)
+
+		return True
+
 
