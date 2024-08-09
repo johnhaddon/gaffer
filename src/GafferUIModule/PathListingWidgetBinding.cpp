334bc71740ef4538631982a95a6f5b9ae2be725a
John Haddon <thehaddonyoof@gmail.com>
2024-08-09 16:03:11 +0100

    PathListingWidget : Improve formatting of Box values

--- src/GafferUIModule/PathListingWidgetBinding.cpp
+++ src/GafferUIModule/PathListingWidgetBinding.cpp
@@ -220,6 +220,16 @@ QString vectorToString( const T &v )
 	return result;
 }
 
+template<typename T>
+QString boxToString( const T &v )
+{
+	if( v.isEmpty() )
+	{
+		return "Empty";
+	}
+	return vectorToString( v.min ) + ", " + vectorToString( v.max );
+}
+
 QVariant dataToVariant( const IECore::Data *value, int role )
 {
 	if( !value )
@@ -337,6 +347,14 @@ QVariant dataToVariant( const IECore::Data *value, int role )
 			return vectorToString( static_cast<const IECore::Color3fData *>( value )->readable() );
 		case IECore::Color4fDataTypeId :
 			return vectorToString( static_cast<const IECore::Color4fData *>( value )->readable() );
+		case IECore::Box2fDataTypeId :
+			return boxToString( static_cast<const IECore::Box2fData *>( value )->readable() );
+		case IECore::Box3fDataTypeId :
+			return boxToString( static_cast<const IECore::Box3fData *>( value )->readable() );
+		case IECore::Box2iDataTypeId :
+			return boxToString( static_cast<const IECore::Box2iData *>( value )->readable() );
+		case IECore::Box3iDataTypeId :
+			return boxToString( static_cast<const IECore::Box3iData *>( value )->readable() );
 		case IECore::DateTimeDataTypeId :
 		{
 			const IECore::DateTimeData *d = static_cast<const IECore::DateTimeData *>( value );
