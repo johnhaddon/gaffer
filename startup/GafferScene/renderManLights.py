c803f13fc51b47ff0c6c0103beaa10c5d2d50b52
John Haddon <thehaddonyoof@gmail.com>
2025-02-10 15:11:15 +0000

    startup : light metadata tweaks

--- startup/GafferScene/renderManLights.py
+++ startup/GafferScene/renderManLights.py
@@ -34,15 +34,11 @@
 #
 ##########################################################################
 
-import math
-import imath
-
 import Gaffer
 
 for type in [
-	"PxrRectLight", "PxrDiskLight", "PxrDistantLight",
-	"PxrDomeLight", "PxrSphereLight", "PxrCyclinderLight",
-	"PxrEnvDayLight"
+	"PxrCyclinderLight", "PxrDomeLight", "PxrDiskLight", "PxrDistantLight",
+	"PxrMeshLight",  "PxrRectLight", "PxrSphereLight",
 ] :
 
 	Gaffer.Metadata.registerValue( "ri:light:" + type, "intensityParameter", "intensity" )
@@ -53,6 +49,7 @@ Gaffer.Metadata.registerValue( "ri:light:PxrCylinderLight", "type", "cylinder" )
 Gaffer.Metadata.registerValue( "ri:light:PxrDomeLight", "type", "environment" )
 Gaffer.Metadata.registerValue( "ri:light:PxrDiskLight", "type", "disk" )
 Gaffer.Metadata.registerValue( "ri:light:PxrDistantLight", "type", "distant" )
+Gaffer.Metadata.registerValue( "ri:light:PxrMeshLight", "type", "mesh" )
 Gaffer.Metadata.registerValue( "ri:light:PxrPortalLight", "type", "portal" )
 Gaffer.Metadata.registerValue( "ri:light:PxrRectLight", "type", "quad" )
 Gaffer.Metadata.registerValue( "ri:light:PxrSphereLight", "type", "point" )
