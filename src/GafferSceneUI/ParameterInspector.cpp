29546fa63984e9b58bd4584ee345a2fcb2d38c4a
Eric Mehl <eric@thinkhypothetical.com>
2025-08-06 15:45:58 -0400

    ParameterInspector : Return parameter input

--- src/GafferSceneUI/ParameterInspector.cpp
+++ src/GafferSceneUI/ParameterInspector.cpp
@@ -70,6 +70,15 @@ IECore::ConstObjectPtr parameterData( const Object *attribute, const ShaderNetwo
 		return nullptr;
 	}
 
+	const ShaderNetwork::Parameter input = parameter.shader.string().empty() ? shaderNetwork->input( { shaderNetwork->getOutput().shader, parameter.name } ) : shaderNetwork->input( parameter );
+	if( input )
+	{
+		return new CompoundData( {
+			{ InternedString( "shaderConnection:shader" ), new InternedStringData( input.shader ) },
+			{ InternedString( "shaderConnection:parameter" ), new InternedStringData( input.name ) }
+		} );
+	}
+
 	const IECoreScene::Shader *shader = parameter.shader.string().empty() ? shaderNetwork->outputShader() : shaderNetwork->getShader( parameter.shader );
 	if( !shader )
 	{
