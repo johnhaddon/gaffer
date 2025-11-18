7c0f0ee3139c564a8bcb40e4013366fc0047c80f
John Haddon <thehaddonyoof@gmail.com>
2025-11-18 12:25:36 +0000

    CI : Build and test against RenderMan 27

---
+++ .github/workflows/main/testGafferRenderMan.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# We are running inside a minimal Rocky container in order to preserve disk
+# space on the GitHub runners. Install the bare minimum of packages required by
+# Gaffer/RenderMan.
+
+microdnf install -y lcms2 mesa-libGL mesa-libGLU fontconfig libgomp
+
+# Run the tests.
+## \todo Add GafferRenderManUITest. This will require additional packages
+# to be installed.
+
+echo "::add-matcher::./.github/workflows/main/problemMatchers/unittest.json"
+$GAFFER_BUILD_DIR/bin/gaffer test IECoreRenderManTest GafferRenderManTest
+echo "::remove-matcher owner=unittest::"
