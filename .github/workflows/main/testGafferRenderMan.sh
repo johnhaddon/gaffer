4300b3ae7513de0f63fdb1ed876fc7eb83676463
John Haddon <thehaddonyoof@gmail.com>
2025-11-18 15:26:38 +0000

    more libs and qt debug

--- .github/workflows/main/testGafferRenderMan.sh
+++ .github/workflows/main/testGafferRenderMan.sh
@@ -6,9 +6,11 @@ set -e
 # space on the GitHub runners. Install the bare minimum of packages required by
 # Gaffer/RenderMan.
 
-microdnf install -y lcms2 mesa-libGL mesa-libGLU libglvnd-opengl fontconfig libgomp libxcb libxkbcommon-x11 xcb-util-image xcb-util-cursor xcb-util-wm xcb-util-keysyms
+microdnf install -y lcms2 mesa-libGL mesa-libGLU libglvnd-opengl fontconfig libgomp libxcb libxkbcommon-x11 xcb-util-image xcb-util-cursor xcb-util-wm xcb-util-keysyms libICE libSM
 
 # Run the tests.
+
+export QT_DEBUG_PLUGINS=1
 
 echo "::add-matcher::./.github/workflows/main/problemMatchers/unittest.json"
 $GAFFER_BUILD_DIR/bin/gaffer test IECoreRenderManTest GafferRenderManTest GafferRenderManUITest
