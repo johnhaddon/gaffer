68ccfbf513c3a7f513352f7589022b648f122032
John Haddon <thehaddonyoof@gmail.com>
2025-11-18 16:35:13 +0000

    no ui tests

--- .github/workflows/main/testGafferRenderMan.sh
+++ .github/workflows/main/testGafferRenderMan.sh
@@ -6,12 +6,10 @@ set -e
 # space on the GitHub runners. Install the bare minimum of packages required by
 # Gaffer/RenderMan.
 
-microdnf install -y lcms2 mesa-libGL mesa-libGLU libglvnd-opengl fontconfig libgomp libxcb libxkbcommon-x11 xcb-util-image xcb-util-cursor xcb-util-wm xcb-util-keysyms libICE libSM
+microdnf install -y lcms2 mesa-libGL mesa-libGLU libglvnd-opengl fontconfig libgomp
 
 # Run the tests.
 
-export QT_DEBUG_PLUGINS=1
-
 echo "::add-matcher::./.github/workflows/main/problemMatchers/unittest.json"
-$GAFFER_BUILD_DIR/bin/gaffer test IECoreRenderManTest GafferRenderManTest GafferRenderManUITest
+$GAFFER_BUILD_DIR/bin/gaffer test IECoreRenderManTest GafferRenderManTest
 echo "::remove-matcher owner=unittest::"
