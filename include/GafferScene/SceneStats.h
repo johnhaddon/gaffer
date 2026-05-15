63f779c5027d70a18745a311cb48b7839f821f0f
John Haddon <thehaddonyoof@gmail.com>
2026-05-15 15:43:42 +0100

    wip

--- include/GafferScene/SceneStats.h
+++ include/GafferScene/SceneStats.h
@@ -51,10 +51,9 @@ namespace GafferScene
 /// TODO
 ///
 /// - Enabled plug per stat?
-/// - Average
+/// - Average - more useful with the above
 /// - Nice UI
 /// - Plug renaming
-/// - Fix serialisation
 class GAFFERSCENE_API SceneStats : public Gaffer::ComputeNode
 {
 
@@ -80,7 +79,6 @@ class GAFFERSCENE_API SceneStats : public Gaffer::ComputeNode
 		Gaffer::ValuePlug *outPlug();
 		const Gaffer::ValuePlug *outPlug() const;
 
-		/// JUST `addInput()`????
 		/// Adds a query whose input is a counterpart of `plug`. Returns the
 		/// newly created input plug, parented to `queriesPlug()`.
 		/// TODO : IS THIS A QUERY OR A STATISTIC? SHOULD NAME BE PASSED LIKE THIS? SHOULD WE SUPPORT ON-THE-FLY RENAMING OF THE PLUG?
